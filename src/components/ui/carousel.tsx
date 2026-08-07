4efa094047d1aa6dd04d2219b79217fb33e52dea
Wealthometer <wealthhajoh87@gmail.com>
2026-08-08 00:11:37 +0100

    Update carousel.tsx

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -21,6 +21,7 @@ type CarouselContextProps = {
   carouselRef: ReturnType<typeof useEmblaCarousel>[0];
   api: ReturnType<typeof useEmblaCarousel>[1];
   scrollPrev: () => void;
+  scrollNext: () => void;  
   canScrollPrev: boolean;
   canScrollNext: boolean;
 } & CarouselProps;
