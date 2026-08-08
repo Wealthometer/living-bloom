c9c1a74b4f73f281100801630c756cc40800fc53
Wealthometer <wealthhajoh87@gmail.com>
2026-08-08 01:00:24 +0100

    Update form.tsx

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -57,7 +57,6 @@ const useFormField = () => {
   return {
     id,
     name: fieldContext.name,
-    formItemId: `${id}-form-item`,
     formDescriptionId: `${id}-form-item-description`,
     formMessageId: `${id}-form-item-message`,
     ...fieldState,
