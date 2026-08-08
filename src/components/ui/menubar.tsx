5389f64bdaa4485e4b06a6155b66263a92ba6488
Wealthometer <wealthhajoh87@gmail.com>
2026-08-08 01:00:48 +0100

    Update menubar.tsx

--- src/components/ui/menubar.tsx
+++ src/components/ui/menubar.tsx
@@ -41,7 +41,6 @@ Menubar.displayName = MenubarPrimitive.Root.displayName;
 
 const MenubarTrigger = React.forwardRef<
   React.ElementRef<typeof MenubarPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Trigger>
 >(({ className, ...props }, ref) => (
   <MenubarPrimitive.Trigger
     ref={ref}
