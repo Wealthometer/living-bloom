2ea4bf4dd1d5a4ab7fc539a855aa747f1ac7aea5
Wealthometer <wealthhajoh87@gmail.com>
2026-08-08 01:01:01 +0100

    Update navigation-menu.tsx

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -52,6 +52,7 @@ const NavigationMenuTrigger = React.forwardRef<
       className="relative top-[1px] ml-1 h-3 w-3 transition duration-300 group-data-[state=open]:rotate-180"
       aria-hidden="true"
     />
+  </NavigationMenuPrimitive.Trigger> 
 ));
 NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName;
 
