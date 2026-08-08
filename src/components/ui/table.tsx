714c965f3018276308dd6e205f9bde5d877544aa
Wealthometer <wealthhajoh87@gmail.com>
2026-08-08 01:02:31 +0100

    Update table.tsx

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -47,6 +47,7 @@ const TableRow = React.forwardRef<HTMLTableRowElement, React.HTMLAttributes<HTML
         "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
         className,
       )}
+      {...props} 
     />
   ),
 );
